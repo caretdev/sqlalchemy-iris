71593c25c29c6da302b986e452f041cbceea5d50
Dmitry Maslennikov <mrdaimor@gmail.com>
2026-05-06 21:29:20 +1000

    clean build

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-packages=("iris" "intersystems_iris" "irisnative")
-for package in ${packages[@]};
-do
-    rm -f ./$package
-    package_path=`python -c "import importlib.util; print(importlib.util.find_spec('${package}').submodule_search_locations[0])"`
-    ln -s $package_path ./$package
-done
-
 set -eo pipefail
 
 PROJECT="$( cd "$(dirname "$0")/.." ; pwd -P )"
@@ -24,11 +16,5 @@ mkdir -p "$PROJECT"/dist
 
 cd "$PROJECT"
 $PYTHON_BIN setup.py sdist bdist_wheel
-
-for package in ${packages[@]};
-do
-    rm -f $package
-done
-rm -rf intersystems-irispython
 
 set +x
